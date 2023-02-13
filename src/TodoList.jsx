c34fe9113ba0222538d45f14052be589664365ed
Godoflaugh <lam.kevin91@gmail.com>
2023-02-13 12:48:31 -0800

    React hooks and effect work

---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TodoList() {
+  return (
+    <>
+      <div>Hello World</div>
+      <div>Hello World</div>
+      <div>Hello World</div>
+    </>
+    
+
+  )
+}
